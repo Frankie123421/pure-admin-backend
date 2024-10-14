b3e4b95fa3754e1be4ea3855e73c509931a0a128
Frankie Lee <frankie@FrankiedeMacBook-Air.local>
2024-10-14 21:40:41 +0800

    feat: add new feature

--- src/router/http.ts
+++ src/router/http.ts
@@ -458,6 +458,62 @@ const captcha = async (req: Request, res: Response) => {
   res.json({ success: true, data: { text: create.text, svg: create.data } });
 };
 
+const asyncRoutes = async (req: Request, res: Response) => {
+  res.json({ 
+    success: true, 
+    data: [
+      {
+        path: "/permission",
+        meta: {
+          title: "权限管理",
+          icon: "lollipop",
+          rank: 10
+        },
+        children: [
+          {
+            path: "/permission/page/index",
+            name: "PermissionPage",
+            meta: {
+              title: "页面权限",
+              roles: ["admin", "common"]
+            }
+          },
+          {
+            path: "/permission/button",
+            meta: {
+              title: "按钮权限",
+              roles: ["admin", "common"]
+            },
+            children: [
+              {
+                path: "/permission/button/router",
+                component: "permission/button/index",
+                name: "PermissionButtonRouter",
+                meta: {
+                  title: "路由返回按钮权限",
+                  auths: [
+                    "permission:btn:add",
+                    "permission:btn:edit",
+                    "permission:btn:delete"
+                  ]
+                }
+              },
+              {
+                path: "/permission/button/login",
+                component: "permission/button/perms",
+                name: "PermissionButtonLogin",
+                meta: {
+                  title: "登录接口返回按钮权限"
+                }
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  });
+};
+
 export {
   login,
   register,
@@ -467,4 +523,5 @@ export {
   searchVague,
   upload,
   captcha,
+  asyncRoutes,
 };
